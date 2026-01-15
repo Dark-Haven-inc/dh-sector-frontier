29f69197c3961c8389a8b6f185c28d38e65a67b4
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2026-01-15 17:31:46 +0300

    test (#5)

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -14,8 +14,8 @@ VERSION = os.environ['GITHUB_SHA']
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.luaworld.ru/"
-FORK_ID = "dsfrontier"
+ROBUST_CDN_URL = "http://dark-haven.xyz:8080/"
+FORK_ID = "dh_test"
 
 def main():
     print("Fetching artifact URL from API...")
